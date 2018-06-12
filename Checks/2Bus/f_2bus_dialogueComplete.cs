c42f65529c22e2b63d32a7894ba74a413ddf48a3
Adam Weesner <klaviam@gmail.com>
2018-06-11 18:45:02 -0700

    Add files via upload

---
+++ Checks/2Bus/f_2bus_dialogueComplete.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class f_2bus_dialogueComplete : MonoBehaviour {
+
+    public GameObject cutscene;
+    public Objects diagComplete; // Holds dialogues that need to be completed
+
+    private bool completed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!completed)
+        {
+            bool done = false;
+
+            // This part checks if all dialogue is complete
+            for (int i = 0; i < diagComplete.objects.Length; i++)
+            {
+                if (!diagComplete.objects[i].GetComponent<Dialogue_Manager>().completed)
+                {
+                    done = false;
+                    break;
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+
+            if (done)
+            {
+                completed = true;
+                cutscene.GetComponent<c_2bus_1>().StartCutscene();
+            }
+        }
+    }
+}
