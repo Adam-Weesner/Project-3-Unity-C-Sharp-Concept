7bcf0ff4e68016220713f44f0c9ce6695291bff5
Adam Weesner <klaviam@gmail.com>
2018-06-11 18:42:09 -0700

    Add files via upload

---
+++ Cutscenes/MainMenu/C_Menu_Intro.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Menu_Intro : MonoBehaviour
+{
+    public GameObject logo;
+    public GameObject menu;
+
+    // Starts opening cutscene
+    void Start()
+    {
+        StartCoroutine(Cutscene0());
+    }
+
+    IEnumerator Cutscene0()
+    {
+        yield return new WaitForSeconds(1);
+
+        // Fade in logo
+        float fadeTime = GetComponent<Cutscene_Fade>().BeginFade(-1);
+        yield return new WaitForSeconds(fadeTime+3);
+        
+        // Fade out logo
+        fadeTime = GetComponent<Cutscene_Fade>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime+3);
+
+        // Disable logo and enable menu
+        logo.SetActive(false);
+        menu.SetActive(true);
+
+        // Fade in menu
+        fadeTime = GetComponent<Cutscene_Fade>().BeginFade(-1);
+        yield return new WaitForSeconds(fadeTime+3);
+
+    }
+}
---
+++ Cutscenes/MainMenu/MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject fadeOut;
+    public AudioSource fadedMusic;
+
+    public void Continue()
+    {
+        // Get choices
+        Test_Choices.choseA = GetBool("choseA");
+
+        // Fade out
+        StartCoroutine(FadeContinue());
+    }
+
+    public void NewGame()
+    {
+        StartCoroutine(FadeNew());
+    }
+
+    bool GetBool(string key)
+    {
+        return PlayerPrefs.GetInt(key) == 1;
+    }
+
+    // Fade audio
+    IEnumerator FadeAudio(AudioSource fadeMusic, float FadeTime)
+    {
+        float startVolume = fadeMusic.volume;
+
+        while (fadeMusic.volume > 0)
+        {
+            fadeMusic.volume -= startVolume * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+
+        fadeMusic.Stop();
+        fadeMusic.volume = startVolume;
+    }
+
+    // Fade out continue
+    IEnumerator FadeContinue()
+    {
+        StartCoroutine(FadeAudio(fadedMusic, 2.5f));
+        float fadeTime = fadeOut.GetComponent<Cutscene_Fade>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime+3);
+
+        SceneManager.LoadScene(PlayerPrefs.GetInt("scene", 1));
+    }
+
+    // Fade out new game
+    IEnumerator FadeNew()
+    {
+        StartCoroutine(FadeAudio(fadedMusic, 2.5f));
+        float fadeTime = fadeOut.GetComponent<Cutscene_Fade>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime+3);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
